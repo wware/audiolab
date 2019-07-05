97ee5575b5068ec1c8fb7191fc1b52c1f281e763
Will Ware <wware@alum.mit.edu>
2019-07-04 23:54:45 -0400

    initial thoughts

---
+++ README.md
@@ -0,0 +1,6 @@
+# Audio stuff
+
+I can use this to prototype digital synthesizers if I create reasonable base classes for audio signals and
+control signals. This would let me deal with fixed-point issues and detect overruns. For just getting
+algorithms to work in the first place, or for purely software synths running on fast processors, I could
+use floating point signals.
---
+++ signals.py
@@ -0,0 +1,17 @@
+import abc
+
+class BaseSignal(object):
+    @abc.abstractmethod
+    def __add__(self):
+        pass
+    @abc.abstractmethod
+    def __sub__(self):
+        pass
+    @abc.abstractmethod
+    def set(self):
+        """Given a number, set the value of this signal"""
+        pass
+    @abc.abstractmethod
+    def value(self):
+        """Return a number for the current value of this signal"""
+        pass
